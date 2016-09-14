f1bb54d8fb525eee0527779f4a4fc2822ea23e32
Andrew Whitley <awhitley1233@gmail.com>
2016-09-14 12:38:59 -0400

    solved using set/clear interval

--- timers.js
+++ timers.js
@@ -1,4 +1,31 @@
+var timer = $('#timer');
+var startButton = $('#start');
+var pauseButton = $('#pause');
+var resetButton = $('#reset');
+
+var seconds = 0;
+var timerId;
+
+function updateTime() {
+  seconds = seconds + 1;
+  timer.text('Time Elapsed: '+seconds);
+}
 
 
+timer.on('click', function() {
 
+});
 
+startButton.on('click', function() {
+  timerId = setInterval(updateTime, 1000);
+});
+
+pauseButton.on('click', function() {
+  clearInterval(timerId);
+});
+
+resetButton.on('click', function() {
+    clearInterval(timerId);
+    seconds = 0;
+    timer.text('Stop Watch');
+});
